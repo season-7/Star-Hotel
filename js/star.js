137ba0f05f5e72792b8e5a908ed0f83e771d5bc7
Brian Lusina <lusinabrian@gmail.com>
2016-06-22 12:38:28 +0300

    Create Reservations page

---
+++ js/star.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    /*bounce effect on social media links at footer*/
+   $('.social').hover(function(){
+      $(this).effect('bounce',{times:2},100);
+   });
+   
+    /*form validation function*/
+});
